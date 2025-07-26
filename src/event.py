9a1c7c720b4e0673ee8c7f763f99bb55edece577
Kevin Chan <kevinmchan@live.com>
2025-07-26 12:49:16 -0400

    Add discriminator event_type field to events

--- src/event.py
+++ src/event.py
@@ -1,20 +1,24 @@
+from typing import Literal
 from pydantic import BaseModel
 
 from .action import Decision
 
 
 class DecisionEvent(BaseModel):
+    event_type: Literal["decision"] = "decision"
     agent_id: str
     round: int
     decision: Decision
 
 
 class StartTurnEvent(BaseModel):
+    event_type: Literal["start_turn"] = "start_turn"
     agent_id: str
     round: int
 
 
 class DeathEvent(BaseModel):
+    event_type: Literal["death"] = "death"
     agent_id: str
     round: int
 
